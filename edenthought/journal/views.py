48f1c80cd1ffa7e206f31a11549dcbdd6e981aef
attilakis-ivan <attila.kisivan@gmail.com>
2024-05-29 08:56:07 +0100

    initial commit

---
+++ edenthought/journal/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+
+from . forms import CreateUserForm
+
+def homepage(request):
+    return render(request, 'journal/index.html')
+
+
+
+
+
+def register(request):
+    
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('my-login')
+    
+    
+    return render(request, 'journal/register.html')
+
+
+
+
+def my_login(request):
+    return render(request, 'journal/my-login.html')
+
+
+
+
+def dashboard(request):
+    return render(request, 'journal/dashboard.html')
+
+
+
+
+
+
+
+
+
+
+
